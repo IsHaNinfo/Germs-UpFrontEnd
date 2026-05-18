5bb403d6f62d240917e7104c07867bcef4d0d612
Ishan <ishaninfo56@gmail.com>
2026-05-18 09:15:24 +0530

    front

--- src/main.tsx
+++ src/main.tsx
@@ -10,48 +10,7 @@ import { AuthProvider } from "@asgardeo/auth-react";
 // import { PermissionProvider } from "./context/PermissionContext.tsx";
 import { UserProvider } from "./context/UserContext.tsx";
 
-// FOR-DEVELOPING
-// const config={
-//   signInRedirectURL: "https://germs-up-front-end.vercel.app",
-//   signOutRedirectURL: "https://germs-up-front-end.vercel.app",
-//   clientID: "OuvFXAdkNq0qcvsFAQcHIR2DVFwa",
-//   baseUrl: "https://api.asgardeo.io/t/germsslaf",
-//   scope: [
-//     "openid",
-//     "profile",
-//     "roles",
-
-//     "dashboard/view_in_sidebar",
-    
-//     "induction/view_in_sidebar",
-//     "induction/view_vehicle_induction",
-//     "induction/view_register_to_airforce",
-//     "induction/view_allocation",
-
-//     "maintenance/view_in_sidebar",
-//     "maintenance/view_servicing",
-//     "maintenance/view_repairs",
-//     "maintenance/view_accident_repairs",
-//     "maintenance/view_utilization_of_mtms",
-//     "maintenance/add_new_service",
-//     "maintenance/edit_service_rule",
-//     "maintenance/add_new_service_rule",
-//     "maintenance/delete_service_rule",
-//     "maintenance/view_service_rule_settings",
-
-//     "mt-operations/view_in_sidebar",
-
-//     "e658/view_in_sidebar",
-//     "e658/view_long_run",
-//     "e658/view_short_run",
-//     "e658/view_rr",
-
-//     "reports/view_in_sidebar",
-
-//     "settings/view_in_sidebar",
-//   ]
-// };
-
+//
 //FOR-GERMS-IS-SERVER
 // const config={
 //   signInRedirectURL: "http://192.168.20.213:8091",
@@ -196,15 +155,15 @@ import { UserProvider } from "./context/UserContext.tsx";
 // };
 
 
-const config={ 
-  signInRedirectURL: "https://germs-up-front-end.vercel.app",
-  signOutRedirectURL: "https://germs-up-front-end.vercel.app",
-  clientID: "EQ42PfKvwO5xeOb1bvo0AykAHcUa",
-  baseUrl: "https://api.asgardeo.io/t/germsslaf56",
-  scope: [
-  "openid profile",
-  ]
-};
+// const config={ 
+//   signInRedirectURL: "https://germs-up-front-end.vercel.app",
+//   signOutRedirectURL: "https://germs-up-front-end.vercel.app",
+//   clientID: "EQ42PfKvwO5xeOb1bvo0AykAHcUa",
+//   baseUrl: "https://api.asgardeo.io/t/germsslaf56",
+//   scope: [
+//   "openid profile",
+//   ]
+// };
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
